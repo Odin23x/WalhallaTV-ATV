ddfa191cc0b584526dd37f8ec2a4f46ca9a82a13
Odin23x <odin23x@walhalla.local>
2026-07-07 02:28:39 +0000

    Fix: verwaisten androidx.compose.material.Text Import entfernt

--- app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/SplashFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/SplashFragment.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
